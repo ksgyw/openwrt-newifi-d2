cffb861978c43e79fd28c690c0c00e7b7f8864f9
ksgyw <97004591+ksgyw@users.noreply.github.com>
2025-08-09 08:55:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/lxiaya/openwrt-homeproxy package/homeproxy
-git clone https://github.com/lxiaya/openwrt-homeproxy package/vlmcsd
+git clone https://github.com/cokebar/openwrt-vlmcsd package/vlmcsd
